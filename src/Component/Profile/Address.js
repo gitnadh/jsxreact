dc0496219c688df608c0c103e09d4dead41a544c
gitnadh <106258053+gitnadh@users.noreply.github.com>
2022-07-17 10:44:54 +0100

    Add files via upload

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,21 @@
+
+
+const Address = () => {
+  return (
+    <>
+  <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+  />
+  <div className="container">
+    <div className="jumbotron">
+      <h1>Address :</h1>
+      <p>I am living in Gafsa, in Hached city Lalla Ksar 2121.</p>
+    </div>
+  </div>
+</>
+
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,25 @@
+
+const FullName = () => {
+  return (
+    <>
+  <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+  />
+  <style
+    dangerouslySetInnerHTML={{
+      __html:
+        "\n  .bg-1 { \n    background-color: #1abc9c; /* Green */\n    color: #ffffff;\n  }\n  "
+    }}
+  />
+  <div className="bg-1">
+    <div className="container text-center">
+      <h3>Nidhal Rebhi</h3>
+      <h3>I'm an student at GoMyCode</h3>
+    </div>
+  </div>
+</>
+  )
+}
+
+export default FullName
